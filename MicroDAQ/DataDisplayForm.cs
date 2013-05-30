62152f5254dc8b24d35336dd06a3d086e5f73838
John <katway@163.com>
2013-05-30 17:44:41 +0800

    更新界面显示的“粒子计数器流量”

--- MicroDAQ/DataDisplayForm.cs
+++ MicroDAQ/DataDisplayForm.cs
@@ -88,14 +88,14 @@ namespace MicroDAQ
                                             meter.ID.ToString()  ,
                                             meter.Value .ToString () ,
                                             string.Empty,
-                                            "流量",
+                                            "粒子计数器流量",
                                             meter.State .ToString (),
                                             meter.Quality.ToString ()}));
                         else
                         {
                             lsvItems.Items[i - 1].SubItems[0].Text = meter.ID.ToString();
                             lsvItems.Items[i - 1].SubItems[1].Text = meter.Value.ToString();
-                            lsvItems.Items[i - 1].SubItems[3].Text = "流量";
+                            lsvItems.Items[i - 1].SubItems[3].Text = "粒子计数器流量";
                             lsvItems.Items[i - 1].SubItems[4].Text = meter.State.ToString();
                             lsvItems.Items[i - 1].SubItems[5].Text = meter.Quality.ToString();
                         }
