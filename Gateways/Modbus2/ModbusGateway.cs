238b76d84653a69f42fb1d25c6c618bc629de747
John <katway@163.com>
2013-09-16 16:38:50 +0800

    在Gateways下增加Modbus2命名空间

---
+++ Gateways/Modbus2/ModbusGateway.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MicroDAQ.Common;
+
+namespace MicroDAQ.Gateways.Modbus2
+{
+    public class ModbusGateway : GatewayBase
+    {
+
+    }
+}
