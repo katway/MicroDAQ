f930afe57a65e3153bcb37ccd690eaa36667314a
John <katway@163.com>
2013-11-04 17:56:07 +0800

    改数据库脚本文件为.sql后缀

---
+++ SqlScript/meters.sql
@@ -0,0 +1,10 @@
+﻿IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[meters]') AND type in (N'U'))
+DROP TABLE [dbo].[meters]
+GO
+
+CREATE TABLE [dbo].[meters](
+	[id] [int] NOT NULL,
+	[type] [int] NULL
+) ON [PRIMARY]
+
+GO
---
+++ SqlScript/meters_value.sql
@@ -0,0 +1,166 @@
+﻿IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[meters_value]') AND type in (N'U'))
+DROP TABLE [dbo].[meters_value]
+GO
+
+CREATE TABLE [dbo].[meters_value] (
+[id] int NOT NULL ,
+[time] datetime NULL ,
+[value1] float(53) NULL ,
+[value2] float(53) NULL ,
+[value3] float(53) NULL ,
+[type] int NULL ,
+[state] int NULL ,
+[zzTime] datetime NULL ,
+[alertTime] datetime NULL ,
+[alert] int NULL ,
+[quality] int NULL ,
+[alertRecord] varchar(32) COLLATE Chinese_PRC_CI_AS NULL 
+)
+ON [PRIMARY]
+GO
+
+CREATE INDEX [_WA_Sys_type_6F7F8B4B] ON [dbo].[meters_value]
+([type] ASC) 
+ON [PRIMARY]
+GO
+
+CREATE INDEX [_WA_Sys_id_6F7F8B4B] ON [dbo].[meters_value]
+([id] ASC) 
+ON [PRIMARY]
+GO
+
+CREATE INDEX [_WA_Sys_time_6F7F8B4B] ON [dbo].[meters_value]
+([time] ASC) 
+ON [PRIMARY]
+GO
+
+CREATE INDEX [_WA_Sys_value1_6F7F8B4B] ON [dbo].[meters_value]
+([value1] ASC) 
+ON [PRIMARY]
+GO
+
+CREATE INDEX [_WA_Sys_value2_6F7F8B4B] ON [dbo].[meters_value]
+([value2] ASC) 
+ON [PRIMARY]
+GO
+
+CREATE INDEX [_WA_Sys_value3_6F7F8B4B] ON [dbo].[meters_value]
+([value3] ASC) 
+ON [PRIMARY]
+GO
+
+CREATE INDEX [_WA_Sys_state_6F7F8B4B] ON [dbo].[meters_value]
+([state] ASC) 
+ON [PRIMARY]
+GO
+
+CREATE INDEX [_WA_Sys_zzTime_6F7F8B4B] ON [dbo].[meters_value]
+([zzTime] ASC) 
+ON [PRIMARY]
+GO
+
+CREATE INDEX [_WA_Sys_alertTime_6F7F8B4B] ON [dbo].[meters_value]
+([alertTime] ASC) 
+ON [PRIMARY]
+GO
+
+CREATE INDEX [_WA_Sys_alert_6F7F8B4B] ON [dbo].[meters_value]
+([alert] ASC) 
+ON [PRIMARY]
+GO
+
+CREATE INDEX [_WA_Sys_quality_6F7F8B4B] ON [dbo].[meters_value]
+([quality] ASC) 
+ON [PRIMARY]
+GO
+
+CREATE INDEX [_WA_Sys_alertRecord_6F7F8B4B] ON [dbo].[meters_value]
+([alertRecord] ASC) 
+ON [PRIMARY]
+GO
+
+CREATE TRIGGER [dbo].[insert_alertRecord_after_meters-value_State_change]
+ON [dbo].[meters_value]
+AFTER UPDATE
+AS
+BEGIN
+  declare @oldState int ,@value float,@newState INT,@ID INT;
+
+             select @oldState =state from deleted;
+	SELECT @newState =state   from inserted;
+	SELECT @ID = id  from inserted;
+	SELECT @value = value1 from inserted;
+                     IF(@oldState!= @newState)
+                            BEGIN
+		IF( @newState <= 2)
+			BEGIN
+                                                DECLARE @alertid VARCHAR(32);
+                                                DECLARE @uuid VARCHAR(32);
+                                                SET @uuid = (SELECT id FROM processitem WHERE slave =@ID);
+                                                SET @alertid = REPLACE(NEWID(), '-', '');
+                                                 
+                                                INSERT INTO ProcessItemAlertRecord (id,processItemId,[timestamp],[value],[confirm])
+                                                                     VALUES(@alertid, @uuid,GETDATE(),@value,'未确认');  
+                                                UPDATE ProcessItem SET alertRecordId = @alertid WHERE slave = @ID;              
+                                           END
+                                END
+END
+GO
+
+IF ((SELECT COUNT(*) from ::fn_listextendedproperty('MS_Description', 
+'USER', N'dbo', 
+'TABLE', N'meters_value', 
+'TRIGGER', N'insert_alertRecord_after_meters-value_State_change')) > 0) 
+EXEC sp_updateextendedproperty @name = N'MS_Description', @value = N'该过程用于向alertRecord表写入仪表掉线后要存储的历史数据'
+, @level0type = 'USER', @level0name = N'dbo'
+, @level1type = 'TABLE', @level1name = N'meters_value'
+, @level2type = 'TRIGGER', @level2name = N'insert_alertRecord_after_meters-value_State_change'
+ELSE
+EXEC sp_addextendedproperty @name = N'MS_Description', @value = N'该过程用于向alertRecord表写入仪表掉线后要存储的历史数据'
+, @level0type = 'USER', @level0name = N'dbo'
+, @level1type = 'TABLE', @level1name = N'meters_value'
+, @level2type = 'TRIGGER', @level2name = N'insert_alertRecord_after_meters-value_State_change'
+GO
+
+CREATE TRIGGER [dbo].[insert_sys-log_after_meters-value_change]
+ON [dbo].[meters_value]
+AFTER UPDATE
+AS
+
+BEGIN
+	declare @oldState INT,@newState INT,@ID INT,@oldQuality INT,@newQuality INT; 
+	--更新前的数据  
+	select @ID = id from meters_value;	
+	select @oldState =state from deleted;--从deleted表中取删除的老名字  
+	select @newState =state from inserted;
+		IF(@oldState!= @newState)
+			BEGIN			
+				INSERT into sys_log (LogDate,LogLevel,LogLogger,LogMessage)   VALUES(getdate(),'state','microdaq','仪表'+convert(varchar(50),@ID)+'状态从'+ convert(varchar(50),@oldState)+'变为'+convert(varchar(50),@newState));
+			END
+	select @oldQuality =Quality from deleted;--从deleted表中取删除的老名字  
+	select @newQuality =Quality from inserted;
+		IF(@oldQuality!= @newQuality)
+			BEGIN			
+				INSERT into sys_log (LogDate,LogLevel,LogLogger,LogMessage)   VALUES(getdate(),'quality','microdaq','仪表'+convert(varchar(50),@ID)+'数据可靠度从'+ convert(varchar(50),@oldQuality)+'变为'+convert(varchar(50),@newQuality));
+			END
+
+END
+
+GO
+
+IF ((SELECT COUNT(*) from ::fn_listextendedproperty('MS_Description', 
+'USER', N'dbo', 
+'TABLE', N'meters_value', 
+'TRIGGER', N'insert_sys-log_after_meters-value_change')) > 0) 
+EXEC sp_updateextendedproperty @name = N'MS_Description', @value = N'应该存储过程用于向sys_log表写入各items的state、quality字段的变化纪录'
+, @level0type = 'USER', @level0name = N'dbo'
+, @level1type = 'TABLE', @level1name = N'meters_value'
+, @level2type = 'TRIGGER', @level2name = N'insert_sys-log_after_meters-value_change'
+ELSE
+EXEC sp_addextendedproperty @name = N'MS_Description', @value = N'应该存储过程用于向sys_log表写入各items的state、quality字段的变化纪录'
+, @level0type = 'USER', @level0name = N'dbo'
+, @level1type = 'TABLE', @level1name = N'meters_value'
+, @level2type = 'TRIGGER', @level2name = N'insert_sys-log_after_meters-value_change'
+GO
+
+GO
