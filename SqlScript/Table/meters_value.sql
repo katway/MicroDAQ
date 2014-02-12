c3dc56b53942fc8f07830bd19d260f76016dcd8d
John <katway@163.com>
2014-02-12 17:51:39 +0800

    修复写报警纪录的触发器

--- SqlScript/Table/meters_value.sql
+++ SqlScript/Table/meters_value.sql
@@ -19,27 +19,25 @@ CREATE TABLE [dbo].[meters_value] (
 ON [PRIMARY]
 GO
 
-
 CREATE INDEX [_WA_Sys_id_6F7F8B4B] ON [dbo].[meters_value]
 ([id] ASC) 
 ON [PRIMARY]
 GO
 
-
-CREATE TRIGGER [dbo].[insert_alertRecord_after_meters-value_State_change]
+CREATE TRIGGER [dbo].[insert_alertRecord_after_meters-value_alert_change]
 ON [dbo].[meters_value]
 AFTER UPDATE
 AS
 BEGIN
-  declare @oldState int ,@value float,@newState INT,@ID INT;
+  declare @oldAlert int ,@value float,@newAlert INT,@ID INT;
 
-             select @oldState =state from deleted;
-	SELECT @newState =state   from inserted;
+             select @oldAlert =alert from deleted;
+	SELECT @newAlert =alert from inserted;
 	SELECT @ID = id  from inserted;
 	SELECT @value = value1 from inserted;
-                     IF(@oldState!= @newState)
+                     IF(@oldAlert!= @newAlert)
                             BEGIN
-								IF( @newState >= 2)
+								IF( @newAlert >= 2)
 									BEGIN
                                                 DECLARE @alertid VARCHAR(32);
                                                 DECLARE @uuid VARCHAR(32);
@@ -48,7 +46,8 @@ BEGIN
                                                  
                                                 INSERT INTO ProcessItemAlertRecord (id,processItemId,[timestamp],[value],[confirm])
                                                                      VALUES(@alertid, @uuid,GETDATE(),@value,'未确认');  
-                                                UPDATE ProcessItem SET alertRecordId = @alertid WHERE slave = @ID;              
+                                                UPDATE ProcessItem SET alertRecordId = @alertid WHERE slave = @ID;
+                                                UPDATE meters_value SET alertRecord =  @alertid WHERE id= @ID;
                                            END
                                        ELSE
                                        		BEGIN
@@ -56,22 +55,9 @@ BEGIN
                                        			UPDATE ProcessItemAlertRecord SET resettime =GETDATE()  WHERE Id = @alertid;
                                        		END
                                 END
-END
-GO
 
-IF ((SELECT COUNT(*) from ::fn_listextendedproperty('MS_Description', 
-'USER', N'dbo', 
-'TABLE', N'meters_value', 
-'TRIGGER', N'insert_alertRecord_after_meters-value_State_change')) > 0) 
-EXEC sp_updateextendedproperty @name = N'MS_Description', @value = N'该过程用于向alertRecord表写入仪表掉线后要存储的历史数据'
-, @level0type = 'USER', @level0name = N'dbo'
-, @level1type = 'TABLE', @level1name = N'meters_value'
-, @level2type = 'TRIGGER', @level2name = N'insert_alertRecord_after_meters-value_State_change'
-ELSE
-EXEC sp_addextendedproperty @name = N'MS_Description', @value = N'该过程用于向alertRecord表写入仪表掉线后要存储的历史数据'
-, @level0type = 'USER', @level0name = N'dbo'
-, @level1type = 'TABLE', @level1name = N'meters_value'
-, @level2type = 'TRIGGER', @level2name = N'insert_alertRecord_after_meters-value_State_change'
+END
+
 GO
 
 CREATE TRIGGER [dbo].[insert_sys-log_after_meters-value_change]
@@ -98,21 +84,5 @@ BEGIN
 
 END
 
-GO
-
-IF ((SELECT COUNT(*) from ::fn_listextendedproperty('MS_Description', 
-'USER', N'dbo', 
-'TABLE', N'meters_value', 
-'TRIGGER', N'insert_sys-log_after_meters-value_change')) > 0) 
-EXEC sp_updateextendedproperty @name = N'MS_Description', @value = N'应该存储过程用于向sys_log表写入各items的state、quality字段的变化纪录'
-, @level0type = 'USER', @level0name = N'dbo'
-, @level1type = 'TABLE', @level1name = N'meters_value'
-, @level2type = 'TRIGGER', @level2name = N'insert_sys-log_after_meters-value_change'
-ELSE
-EXEC sp_addextendedproperty @name = N'MS_Description', @value = N'应该存储过程用于向sys_log表写入各items的state、quality字段的变化纪录'
-, @level0type = 'USER', @level0name = N'dbo'
-, @level1type = 'TABLE', @level1name = N'meters_value'
-, @level2type = 'TRIGGER', @level2name = N'insert_sys-log_after_meters-value_change'
-GO
 
 GO
